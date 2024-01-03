3af42d3d9dc74a60d794efe56d6fd8cd82d4ef5c
kenkmkm <125950184+kenkmkm@users.noreply.github.com>
2024-01-04 00:12:22 +0800

    test
    test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temporary Page</title>
+</head>
+<body>
+    <h1>Temporary Page</h1>
+    <h3><a href="/frontend/index.php">Frontend</a></h3>
+    <h3><a href="/backend/index.php">Backend</a></h3>
+</body>
+</html>
